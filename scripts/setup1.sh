5eb13237435878c0a436f0afce2419a74b6f47c2
Michael Predko <myke.predko@mimetics.ca>
2025-08-06 00:30:18 -0400

    Update setup1.sh

--- scripts/setup1.sh
+++ scripts/setup1.sh
@@ -312,7 +312,7 @@ wget -O cycle38.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/
 wget -O cycleRESET.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/cycleRESET.py?raw=true
 wget -O enableDFU.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/enableDFU.py?raw=true
 wget -O enableKatapult.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/enableKatapult.py?raw=true
-wget -O enableKatapult.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/gpioread.py?raw=true
+wget -O gpioread.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/gpioread.py?raw=true
 wget -O toggleBOOT0.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/toggleBOOT0.py?raw=true
 chmod 777 *.py
 cd ~
