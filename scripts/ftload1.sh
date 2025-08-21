9c426fedaae8ae9630dc648a2f179e1697534162
Michael Predko <myke.predko@mimetics.ca>
2025-08-21 00:17:41 -0400

    Create ftload1.sh

---
+++ scripts/ftload1.sh
@@ -0,0 +1,33 @@
+# Steps 22 through 26
+mkdir python
+cd python
+wget -O cycle38.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/cycle38.py?raw=true
+wget -O cycleRESET.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/cycleRESET.py?raw=true
+wget -O enableDFU.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/enableDFU.py?raw=true
+wget -O enableKatapult.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/enableKatapult.py?raw=true
+wget -O gpioread.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/gpioread.py?raw=true
+wget -O toggleBOOT0.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/python/toggleBOOT0.py?raw=true
+chmod 777 *.py
+cd ~
+mkdir scripts
+cd scripts
+wget -O ft.sh https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/scripts/ft.sh?raw=true
+chmod 777 *.sh
+cd ~
+mkdir logs
+mkdir bin
+cd bin
+wget -O nada.bin https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/bin/nada.bin?raw=true
+wget -O KGP_4x2209_DFU.bin https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/bin/KGP_4x2209_DFU.bin?raw=true
+cd ~
+# Steps 35, 36 and 40
+git clone https://github.com/dw-0/kiauh.git
+git clone https://github.com/Arksine/katapult
+./kiauh/kiauh.sh
+# Steps 27 through 34 follow
+# Step 37 (Reboot)
+# Continue from Step 43
+# Reverse Steps 57 and 58 (might as well force the board into DFU Mode)
+# Step 69: Note that printer.cfg can also be cut and pasted into printer.cfg on Mainsail
+# Step 71: ./scripts/loaddynamicmacros.sh
+# Step 74: ./scripts/installdynamicmacros.sh
