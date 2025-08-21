7343809fe2b30d4bbe3e9ce7a1f400739eb6d717
Michael Predko <myke.predko@mimetics.ca>
2025-08-21 01:27:17 -0400

    Create ftload2.sh

---
+++ scripts/ftload2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to simplify Functional Test Micro SD Card setup
+# Execute with "curl -s https://raw.githubusercontent.com/3dApothecary-xyz/FunctionalTest/refs/heads/main/scripts/ftload2.sh | bash"
+cd ~/printer_data/config
+wget -O dynamic.cfg https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/configs/dynamic.cfg?raw=true
+wget -O read_adxl.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/configs/read_adxl.py?raw=true
+wget -O read_blt.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/configs/read_blt.py?raw=true
+wget -O read_tmc_field.py https://github.com/3dApothecary-xyz/FunctionalTest/blob/main/configs/read_tmc_field.py?raw=true
+chmod 777 dynamic.cfg
+chmod 777 *.py
+cd ~
