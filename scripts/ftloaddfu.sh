60d3e59a89c5cbf2d19d7b087b98aaa30b7d2359
Michael Predko <myke.predko@mimetics.ca>
2025-09-26 17:03:29 -0400

    Create ftloaddfu.sh

---
+++ scripts/ftloaddfu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to simplify Functional Test Micro SD Card setup
+# Execute with "curl -s https://raw.githubusercontent.com/3dApothecary-xyz/FunctionalTest/refs/heads/main/scripts/ftloaddfu.sh | bash"
+
+expectedSerialDevResponse="usb-katapult_stm32g0b1xx_"
+serialDevResponse=$(ls /dev/serial/by-id)
+echo -e "\"$serialDevResponse\""
+
+if [[ "${serialDevResponse:0:25}" != "$expectedSerialDevResponse" ]]; then
+  echo -e "  "
+  echo -e "Invalid Response to \"ls /dev/serial/by-id\""
+  exit
+fi
+
+python3 ~/katapult/scripts/flashtool.py -f ~/bin/KGP_4x2209_DFU.bin -d /dev/serial/by-id/$serialDevResponse
