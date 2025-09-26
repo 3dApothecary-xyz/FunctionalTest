c2579e2b60094460d8347ff5a9f1c14d5eb4f4aa
Michael Predko <myke.predko@mimetics.ca>
2025-09-25 23:36:27 -0400

    Create cycle40.py

---
+++ python/cycle40.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Includes
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitions
+TESTPIN = 40
+
+# Enable IO Pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(TESTPIN, GPIO.OUT)
+
+
+# Application Functionality
+for i in range(20):
+  GPIO.output(TESTPIN, GPIO.HIGH)
+  time.sleep(2)
+  GPIO.output(TESTPIN, GPIO.LOW)
+  GPIO.sleep(2)
+
+# Restore Pin State
+GPIO.cleanup()
